574b21f62a8a912dbf2a6e6b2d003de49cc9e88e
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-01-04 10:54:28 -0300

    Dia35

---
+++ linguagem-sql.py/sql-direto-banco-dados.py
@@ -0,0 +1,29 @@
+import sqlite3
+import pandas as pd
+
+con = sqlite3.connect('C:/Users/Sabrina Gama/Downloads/python/bibliotecas/linguagem-sql.py/estudos/cap12_dsa.db')
+cursor = con.cursor()
+
+# Listar todas as tabelas no banco de dados
+sql_query = """SELECT name FROM sqlite_master WHERE type = 'table';"""
+cursor.execute(sql_query)
+print('Tabelas no banco de dados:')
+print(cursor.fetchall())
+print('-=' * 40)
+
+# Acessar a tabela tb_vendas_dsa
+query1 = 'SELECT * FROM tb_vendas_dsa'
+cursor.execute(query1)
+
+# Obter nomes das colunas
+nomes_colunas = [description[0] for description in cursor.description]
+print('Nomes das colunas: ')
+print(nomes_colunas)
+print('-=' * 40)
+
+# Obter dados da tabela
+dados = cursor.fetchall()
+print('Dados da TABELA até os 10 primeiros: ')
+print(dados[:10])
+print('-=' * 40)
+
